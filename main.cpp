ba906e68edd101c11c06b4fe7304f30f53688d23
Sadteeto <87579232+Sadteeto@users.noreply.github.com>
2021-12-02 00:14:29 +1000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,11 +8,23 @@ int16_t sensor_value_old = 0;
 int16_t sensor_value_diff = 0;
 int16_t sensor_value_diff_old = 0;
 int8_t sensor_value_diff_diff = 0;
+bool takeoff = false;
+const int reed_switch = 2;
 
 void setup() {
-  
-  Serial.begin(9600);
+  pinMode(reed_switch, INPUT);
 
+  if (digitalRead(reed_switch)/*the switch is pulled up*/) {
+    while (1)/*Dump it all*/
+    {
+      Serial.begin(9600);
+      for(eeprom_address = 0; eeprom_address < EEPROM.length(); eeprom_address++) {
+        Serial.print(EEPROM.read(eeprom_address), DEC);
+        Serial.print(",");
+      }
+      delay(15000);
+    }
+  }
 
   // TIMER 1 for interrupt frequency 10 Hz:
   cli(); // stop interrupts
@@ -42,43 +54,47 @@ ISR(TIMER1_COMPA_vect) {
   // get height
   sensor_value = {
     // Sensor read here
-    // could be in meter(+-128m/s) or in dm(+-12.8m/s)
     0
   };
-  
-  // calculate rocket velocity
-  sensor_value_diff = sensor_value - sensor_value_old;
-  
-  // calculate rocket acceletion
-  sensor_value_diff_diff = sensor_value_diff - sensor_value_diff_old;
-  
-  // store acceletion in EEPROM
-  // we could figure out the displacement by deriving the acceleration
-  EEPROM.update(eeprom_address, sensor_value_diff_diff);
-  
-  // shift the 'C's
-  sensor_value_old = sensor_value;
-  sensor_value_diff_old = sensor_value_diff;
 
-  eeprom_address++;
-  i++;
-  //print i
-  Serial.println(i, DEC); // print the value of i
+  // takeoff detection
+  if ((sensor_value > sensor_value_old + 2) && takeoff == 0) {
+    // send takeoff signal
+    takeoff = 1;
+  }
+  else{
+    // calculate rocket velocity
+    sensor_value_diff = sensor_value - sensor_value_old;
+    
+    // calculate rocket acceletion
+    sensor_value_diff_diff = sensor_value_diff - sensor_value_diff_old;
+    
+    // store acceletion in EEPROM
+    // we could figure out the displacement by deriving the acceleration
+    EEPROM.update(eeprom_address, sensor_value_diff_diff);
+    
+    // shift the 'C's
+    sensor_value_old = sensor_value;
+    sensor_value_diff_old = sensor_value_diff;
 
-  // break out of the loop if we have reached the end of the EEPROM
-  if (i >= EEPROM.length()) {
-    // turn off timer 1
-    TCCR1B = 0;
-    // turn off timer 1 interrupt
-    TIMSK1 = 0;
+    eeprom_address++;
+    i++;
+
+    // break out of the loop if we have reached the end of the EEPROM
+    if (i >= EEPROM.length()) {
+      // turn off timer 1
+      TCCR1B = 0;
+      // turn off timer 1 interrupt
+      TIMSK1 = 0;
 
 
-    // optionally print the EEPROM content
+      // optionally print the EEPROM content (for debugging)
 
-    // for(eeprom_address = 0; eeprom_address < EEPROM.length(); eeprom_address++) {
-    //   Serial.print(EEPROM.read(eeprom_address), DEC);
-    //   Serial.print(" ");
-    // }
+        // for(eeprom_address = 0; eeprom_address < EEPROM.length(); eeprom_address++) {
+        //   Serial.print(EEPROM.read(eeprom_address), DEC);
+        //   Serial.print(" ");
+        // }
 
+    }
   }
 }
